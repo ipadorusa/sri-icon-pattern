d11d83e82568467b15b21a1062ddffb9466929bd
ipadorusa <ipadorusa@gmail.com>
2018-12-21 17:55:20 +0900

    object add

--- src/components/icons/IconMobile.vue
+++ src/components/icons/IconMobile.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div class="icon-box" v-for="(icon, index) in icons" :key="icon.name">
-            <div class="ico" :class="`${icons[index].name} ${icons[index].size}`"></div>
+            <div class="icon" :class="`${icons[index].name} ${icons[index].size}`"></div>
         </div>
     </div>
 </template>
@@ -13,9 +13,16 @@ export default {
   	return {
       icons: [
         {'name': 'search', 'size': 'icoSizeXL'},
-        {'name': 'magnify', 'size': 'icoSizeXL'},
-        {'name': 'minify', 'size': 'icoSizeXL'},
-        {'name': 'close', 'size': 'icoSizeXL'}
+        {'name': 'close', 'size': 'icoSizeXL'},
+        {'name': 'arrow-left', 'size': 'icoSizeXL'},
+        {'name': 'arrow-right', 'size': 'icoSizeXL'},
+        {'name': 'arrow-up', 'size': 'icoSizeXL'},
+        {'name': 'arrow-down', 'size': 'icoSizeXL'},
+        {'name': 'plus', 'size': 'icoSizeXL'},
+        {'name': 'minus', 'size': 'icoSizeXL'},
+        {'name': 'check', 'size': 'icoSizeXL'},
+        {'name': 'remove', 'size': 'icoSizeXL'}
+
       ]
     }
   }
